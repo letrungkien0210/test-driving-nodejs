0a3206b871cd8ec10599f72f35409d316d52330c
Kien Le <letrungkien0210@gmail.com>
2019-07-26 06:35:53 +0700

    test-driving initial

---
+++ src/stockfetch.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const Stockfetch = function() {
+    this.readTickersFile = (filename, onError) => {
+        const self = this;
+
+        const processResponse = function(err, data) {
+            if (err) 
+                onError('Error reading file: ' + filename);
+            else {
+                const tickers = self.parseTickers(data.toString());
+                if(tickers.length === 0) {
+                    onError(`File ${filename} has invalid content`);
+                } else {
+                    self.processTickers(tickers);
+                }
+            }
+        };
+
+        fs.readFile(filename, processResponse);
+    };
+
+    this.parseTickers = function(content){
+        const isInRightFormat = function(str){
+            return str.trim().length !==0 && str.indexOf(' ') < 0;
+        };
+        return content.split('\n').filter(isInRightFormat);
+    };
+    this.processTickers = function(tickers){
+        const self = this;
+        self.tickersCount = tickers.length;
+        tickers.forEach(function(ticker) {
+            self.getPrice(ticker);
+        });
+    };
+
+    this.tickersCount = 0;
+
+    this.getPrice = function(){};
+};
+
+module.exports = Stockfetch;
---
+++ test/stockfetch-test.js
@@ -0,0 +1,102 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const fs = require('fs');
+const Stockfetch = require('./../src/stockfetch');
+
+describe('Stockfetch tests', function() {
+    let stockfetch, sandbox;
+
+    this.beforeEach(function() {
+        stockfetch = new Stockfetch();
+        sandbox = sinon.sandbox.create();
+    })
+
+    this.afterEach(function() {
+        sandbox.restore();
+    })
+
+    it('Should pass this canary test', function(){
+        expect(true).to.be.true;
+    })
+
+    it('read should invoke error handler for invalid file', function(done) {
+        const onError = (err) => {
+            expect(err).to.be.eql('Error reading file: InvalidFile');
+            done();
+        }
+
+        sandbox.stub(fs, 'readFile', function(fileName, callback) {
+            callback(new Error('failed'))
+        });
+
+        stockfetch.readTickersFile('InvalidFile', onError);
+    })
+
+    it('read should invoke processTickers for valid file', function(done) {
+        const rawData = "GOOG\nAAPL\nORCL\nMSFT";
+        const parsedData = ['GOOG', 'AAPL', 'ORCL', 'MSFT'];
+
+        sandbox.stub(stockfetch, 'parseTickers')
+        .withArgs(rawData).returns(parsedData);
+
+        sandbox.stub(stockfetch, 'processTickers', function(data) {
+            expect(data).to.be.eql(parsedData);
+            done();
+        });
+
+        sandbox.stub(fs, 'readFile', function(fileName, callback) {
+            callback(null, rawData);
+        })
+
+        stockfetch.readTickersFile('tickers.txt')
+    })
+
+    it('read should return error if given file is empty', function(done) {
+        const onError = function(error){
+            expect(error).to.be.eql('File tickers.txt has invalid content');
+            done();
+        }
+
+        sandbox.stub(stockfetch, 'parseTickers').withArgs('').returns([]);
+
+        sandbox.stub(fs, 'readFile', function(fileName, callback) {
+            callback(null, '');
+        });
+
+        stockfetch.readTickersFile('tickers.txt', onError);
+    })
+
+    it('parseTickers should return tickers', function() {
+        expect(stockfetch.parseTickers("A\nB\nC")).to.be.eql(['A', 'B', 'C']);
+    })
+
+    it('parseTickers should return empty array for empty content', function() {
+        expect(stockfetch.parseTickers('')).to.be.eql([]);
+    })
+
+    it('parseTickers should return empty array for white-space', function() {
+        expect(stockfetch.parseTickers("  ")).to.be.eql([]);
+    })
+
+    it('parseTickers should ignore unexpected format on content', function() {
+        const rawData = "AAPL \nBla h\nGOOG\n\n ";
+        expect(stockfetch.parseTickers(rawData)).to.be.eql(['GOOG']);
+    })
+
+    it('processTickers should call getPrice for each ticker symbol', function() {
+        const stockfetchMock = sandbox.mock(stockfetch);
+        stockfetchMock.expects('getPrice').withArgs('A');
+        stockfetchMock.expects('getPrice').withArgs('B');
+        stockfetchMock.expects('getPrice').withArgs('C');
+
+        stockfetch.processTickers(['A', 'B', 'C']);
+        stockfetchMock.verify();
+    })
+
+    it('processTickers should save tickers count', function() {
+        sandbox.stub(stockfetch, 'getPrice');
+
+        stockfetch.processTickers(['A', 'B', 'C']);
+        expect(stockfetch.tickersCount).to.be.eql(3);
+    })
+});
