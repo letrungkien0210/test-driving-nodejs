980015ce50c8404e3c7a2db579fb37c1d8ae826b
Kien Le <letrungkien0210@gmail.com>
2019-07-30 06:54:10 +0700

    Changed another endpoint of Yahoo services. Updated the tests script.

--- src/stockfetch.js
+++ src/stockfetch.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const http = require('http');
+const http = require('https');
 
 const Stockfetch = function() {
     this.getPriceForTickers = function(fileName, displayFn, errorFn) {
@@ -32,6 +32,7 @@ const Stockfetch = function() {
         };
         return content.split('\n').filter(isInRightFormat);
     };
+
     this.processTickers = function(tickers){
         const self = this;
         self.tickersCount = tickers.length;
@@ -42,14 +43,21 @@ const Stockfetch = function() {
 
     this.tickersCount = 0;
 
-    this.getPrice = function(){};
-
     this.http = http;
 
     this.getPrice = function(symbol) {
-        const url = `http://ichart.finance.yahoo.com/table.csv?s=${symbol}`;
-        this.http.get(url, this.processResponse.bind(this, symbol))
+        const options = {
+            host: 'query1.finance.yahoo.com',
+            path: `/v7/finance/download/${symbol}?period1=1532563869&period2=1564099869&interval=1d&events=history&crumb=UZCvuJz9gPx`,
+            method: 'GET',
+            headers: {
+                "Cookie": 'APID=UP25707bc1-6911-11e9-8ad3-02b779cd917c; T=z=MYrHdBMsSMdBSBwlOb5jqNOMzc2MQY1Mk9OMjFOME8wMDM3Mj&a=QAE&sk=DAAQZ1g3g9Yjg2&ks=EAAxXLi3siz6VyFXBEtk4E0Hg--~G&kt=EAAdAgWK9KgnKlwhRSsE9w7Dw--~I&ku=FAAnDl3kDOyiB0n.c8lEyDKLxycoiSAmG5hNU6mK1DMEvkRjWqhY941tat5fmodYSOVmqLISsljFrgqAaA8ZoseDtwKqw06OzT1lFxddiEkeRzmH7AjOkkWIjlyUQ0oA_hspqw5uBdXXyCfKXg7XOiA3tgrvbMnkfVkpmdwwlDApmE-~A&d=bnMBeWFob28BZwFFQlFJQ0JVUzNMSldDUlhBSUQ3TzJBRDVFWQFzbAFOREF4TmdFeU5UZzVOVFk1TnpnM056UXdOVEF6T0EtLQFhAVFBRQFhYwFBRUdiZTNLYgFsYXQBTVlySGRCAWNzAQFzYwFkZXNrdG9wX3dlYgFmcwF2NFVWeWxWZEhyWU0BenoBTVlySGRCQTdF&af=JnRzPTE1NjIyOTM3NzImcHM9OUpWOTY2b1A0Y1gwdWMxWnBKZE9RZy0t; F=d=0Ppy0bA9vKWJBEJyhYEaG7vU49aVWrm8eFGVhHSPow--; PH=fn=RQlnCBB73Ll.66nPDpk-&l=en-US&i=us; Y=v=1&n=4jlhjs5gdn6uv&l=a42e3ed_a4d/o&p=m2p0c1p00000000&r=k4&intl=us; AO=u=1; GUC=AQEAAQJdH_1d-kIaNwOv&s=AQAAACJFygd2&g=XR62Gw; ucs=tr=1564185033000; PRF=t%3DBTC-USD; APIDTS=1564100262; B=285n7ldec9405&b=4&d=bqsnDtxpYELbCAxJF0hTsj50xuXM048GIzGE4Q--&s=lu&i=goplCBeh.vm6fFNkpa7M'
+            }
+        }
+        const req = http.request(options, this.processResponse.bind(this, symbol))
         .on('error', this.processHttpError.bind(this, symbol));
+
+        req.end();
     };
 
     this.processResponse = function (symbol, response) {
@@ -93,7 +101,9 @@ const Stockfetch = function() {
     }
 
     this.reportCallback = function() {};
-    this.processHttpError = function(){};
+    this.processHttpError = function(ticker, error){
+        this.processError(ticker, error.code);
+    };
 
 };
 
--- test/stockfetch-test.js
+++ test/stockfetch-test.js
@@ -101,8 +101,15 @@ describe('Stockfetch tests', function() {
     })
 
     it('getPrice should call get on http with valid URL',  function(done) {
-        const httpSub = sandbox.stub(stockfetch.http, 'get', function(url) {
-            expect(url).to.be.eql('http://ichart.finance.yahoo.com/table.csv?s=GOOG');
+        const httpSub = sandbox.stub(stockfetch.http, 'request', function(options) {
+            expect(options).to.deep.eql({
+                host: 'query1.finance.yahoo.com',
+                    path: `/v7/finance/download/GOOG?period1=1532563869&period2=1564099869&interval=1d&events=history&crumb=UZCvuJz9gPx`,
+                    method: 'GET',
+                    headers: {
+                        "Cookie": 'APID=UP25707bc1-6911-11e9-8ad3-02b779cd917c; T=z=MYrHdBMsSMdBSBwlOb5jqNOMzc2MQY1Mk9OMjFOME8wMDM3Mj&a=QAE&sk=DAAQZ1g3g9Yjg2&ks=EAAxXLi3siz6VyFXBEtk4E0Hg--~G&kt=EAAdAgWK9KgnKlwhRSsE9w7Dw--~I&ku=FAAnDl3kDOyiB0n.c8lEyDKLxycoiSAmG5hNU6mK1DMEvkRjWqhY941tat5fmodYSOVmqLISsljFrgqAaA8ZoseDtwKqw06OzT1lFxddiEkeRzmH7AjOkkWIjlyUQ0oA_hspqw5uBdXXyCfKXg7XOiA3tgrvbMnkfVkpmdwwlDApmE-~A&d=bnMBeWFob28BZwFFQlFJQ0JVUzNMSldDUlhBSUQ3TzJBRDVFWQFzbAFOREF4TmdFeU5UZzVOVFk1TnpnM056UXdOVEF6T0EtLQFhAVFBRQFhYwFBRUdiZTNLYgFsYXQBTVlySGRCAWNzAQFzYwFkZXNrdG9wX3dlYgFmcwF2NFVWeWxWZEhyWU0BenoBTVlySGRCQTdF&af=JnRzPTE1NjIyOTM3NzImcHM9OUpWOTY2b1A0Y1gwdWMxWnBKZE9RZy0t; F=d=0Ppy0bA9vKWJBEJyhYEaG7vU49aVWrm8eFGVhHSPow--; PH=fn=RQlnCBB73Ll.66nPDpk-&l=en-US&i=us; Y=v=1&n=4jlhjs5gdn6uv&l=a42e3ed_a4d/o&p=m2p0c1p00000000&r=k4&intl=us; AO=u=1; GUC=AQEAAQJdH_1d-kIaNwOv&s=AQAAACJFygd2&g=XR62Gw; ucs=tr=1564185033000; PRF=t%3DBTC-USD; APIDTS=1564100262; B=285n7ldec9405&b=4&d=bqsnDtxpYELbCAxJF0hTsj50xuXM048GIzGE4Q--&s=lu&i=goplCBeh.vm6fFNkpa7M'
+                    }
+            })
             done();
             return { on: function() {}};
         })
@@ -117,7 +124,7 @@ describe('Stockfetch tests', function() {
         .withArgs(stockfetch, 'GOOG')
         .returns(aHandler);
 
-        const httpStub = sandbox.stub(stockfetch.http, 'get', function(url, handler) {
+        const httpStub = sandbox.stub(stockfetch.http, 'request', function(url, handler) {
             expect(handler).to.be.eql(aHandler);
             done();
             return { on: function() {}};
@@ -139,7 +146,7 @@ describe('Stockfetch tests', function() {
             done();
         }
 
-        sandbox.stub(stockfetch.http, 'get').returns({ on: onStub});
+        sandbox.stub(stockfetch.http, 'request').returns({ on: onStub});
 
         stockfetch.getPrice('GOOG');
     })
@@ -189,6 +196,16 @@ describe('Stockfetch tests', function() {
         .never();
 
         stockfetch.processResponse('GOOG', response);
+        processErrorMock.verify();
+    })
+
+    it('processHttpErro should call processError with error details', function () {
+        const processErrorMock = sandbox.mock(stockfetch)
+        .expects('processError')
+        .withArgs('GOOG', '...error code...');
+
+        const error = { code: '...error code...' };
+        stockfetch.processHttpError('GOOG', error);
         processErrorMock.verify();
     })
 
@@ -285,7 +302,7 @@ describe('Stockfetch tests', function() {
 
         const display = function(prices, errors) {
             expect(prices.length).to.be.eql(4);
-            expect(error.length).to.be.eql(4);
+            expect(errors.length).to.be.eql(1);
             onError.verify();
             done();
         }
